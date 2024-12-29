d13bb0a9bdb345f13b854a4d642b738d4ecc637f
SajadPiran <sajadpiran7@gmail.com>
2024-12-29 02:48:55 -0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+
+      colors: {
+        primary: '#B9FF66',
+        secondary: '#191A23',
+        third : '#F3F3F3'
+      }
+
+    },
+  },
+  plugins: [],
+}
+
